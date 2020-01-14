feb3cd151d2d1849fd71dcc6d2b3645a895dfde6
Lucas Dias <lucas.mendonca159@gmail.com>
2020-01-14 00:50:46 -0300

    Day One

---
+++ day 1/src/DayOne.java
@@ -0,0 +1,31 @@
+public class DayOne {
+    public static void main(String[]args) { // The main method is the first one to be executed
+        //Data Types
+        //Primitive
+        byte a = 90;
+        short b = 20500;
+        int c = 150000;
+        long d = 1000000;
+        boolean e = false;
+        float f = (float) 2.0;
+        double g = 1500.00;
+
+        //Non-primitive data types (two examples only)
+        String h = "Test";
+        Integer i = 1200;
+
+        //Final variable, which means it can no longer be edited
+        final double j = 3.4f;
+
+        //The PrintLn method
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+    }
+}

6972cb5869ae5c7d992ac8d8595b394e4420e95a
Lucas Dias <lucas.mendonca159@gmail.com>
2020-01-15 23:20:19 -0300

    Updates Methods.java

--- day 3/src/Methods.java
+++ day 3/src/Methods.java
@@ -1,2 +1,28 @@
-public class Methods {
+public class Methods { //Getters & Setters will be used in this exercise
+    private long anyNumber;
+
+    public static void main (String[] args) {
+        Methods method = new Methods(); //Instantiates a new Methods object
+        long randomNumber = (long) (Math.random() * 100); //Generates a random number
+        method.setAnyNumber(randomNumber);
+        method.printNumberOnConsole(method.getAnyNumber());
+    }
+
+    public void setAnyNumber(long anyNumber) {
+        this.anyNumber = anyNumber;
+    }
+
+    public long getAnyNumber() {
+        return anyNumber;
+    }
+
+    public void printNumberOnConsole(long randomNumber) {
+        if(anyNumber > 75) {
+            System.out.println("Woah! You got a pretty high number");
+        } else if(anyNumber > 50) {
+            System.out.println("You got an OK number!");
+        } else {
+            System.out.println("Damn, good luck next time...");
+        }
+    }
 }

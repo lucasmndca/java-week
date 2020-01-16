d80d20a02deb2766883cf0e35e5f25b8b18b405d
Lucas Dias <lucas.mendonca159@gmail.com>
2020-01-15 23:01:59 -0300

    Creates Classes.java

---
+++ day 3/src/Classes.java
@@ -0,0 +1,7 @@
+public class Classes {
+    int number = 500; //Declares a class attribute and assign the value 500 to it
+    public static void main (String[] args) {
+        Classes classObj = new Classes(); //Instantiates a new Classes object
+        System.out.println(classObj.number); //Gets the attribute 'number' from 'Classes' class and prints it.
+    }
+}
